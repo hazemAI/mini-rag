13f7e24025b5f39d1411d3119eaca64910e1d766
hazemAI <11410120210111@stud.cu.edu.eg>
2025-10-27 17:28:03 +0300

    fix(data-router): fix id type errors

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, APIRouter, Depends, UploadFile, status, Request
+from fastapi import APIRouter, Depends, UploadFile, status, Request
 from fastapi.responses import JSONResponse
 import os
 import aiofiles
@@ -9,7 +9,9 @@ from models import ResponseSignal
 from .schemas import ProcessRequest
 from models.ProjectModel import ProjectModel
 from models.ChunkModel import ChunkModel
-from models.db_schemas import DataChunk
+from models.AssetModel import AssetModel
+from models.db_schemas import DataChunk, Asset
+from models.enums.AssetTypeEnum import AssetTypeEnum
 
 
 logger = logging.getLogger("uvicorn.error")
@@ -64,10 +66,23 @@ async def upload_data(request: Request, project_id: str, file: UploadFile,
             }
         )
 
+    # store assets into db
+    asset_model = await AssetModel.create_instance(
+        db_client=request.app.db_client
+    )
+    
+    asset_resource = Asset(
+        asset_project_id=project.id,
+        asset_type=AssetTypeEnum.FILE.value,
+        asset_name=file_id,
+        asset_size=os.path.getsize(file_path)
+    )
+    asset_record = await asset_model.create_asset(asset=asset_resource)
+
     return JSONResponse(
         content={
             "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
-            "file_id": file_id,
+            "file_id": str(asset_record.id),
         }
     )
 
